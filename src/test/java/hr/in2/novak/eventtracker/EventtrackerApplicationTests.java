49f5d411c56fbefc3b8c6b146c082ed9c16d841c
tompa402 <tomislavno@gmail.com>
2019-05-15 18:36:00 +0200

    Initial commit

---
+++ src/test/java/hr/in2/novak/eventtracker/EventtrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package hr.in2.novak.eventtracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EventtrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
