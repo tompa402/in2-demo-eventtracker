6d66d3de16649d874586e63363e59c1e64f8a0a2
tompa402 <tomislavno@gmail.com>
2019-05-15 18:34:32 +0200

    Initial commit

---
+++ src/main/java/hr/in2/novak/eventtracker/EventtrackerApplication.java
@@ -0,0 +1,13 @@
+package hr.in2.novak.eventtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventtrackerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/hr/in2/novak/eventtracker/ServletInitializer.java
@@ -0,0 +1,13 @@
+package hr.in2.novak.eventtracker;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EventtrackerApplication.class);
+	}
+
+}
